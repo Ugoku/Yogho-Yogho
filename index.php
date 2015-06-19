09a1cdf7996fc7b6104dd8b3bf9f7cc052725aba
Sander <github@ugoku.nl>
2015-06-19 22:38:08 +0200

    Update example script with tiles

--- index.php
+++ index.php
@@ -4,7 +4,12 @@
 	include('Classes\Offsets.php');
 
 	include('Classes\Palette.php');
+	include('Classes\Tiles.php');
 
 	// Show the palette for the title screens
-	$palette = new Yogho\Palette;
-	$palette->toImage();
+	/*$palette = new Yogho\Palette;
+	$palette->toImage();*/
+
+	// Show the tiles for the first level
+	$tiles = new Yogho\Tiles(1);
+	$tiles->toImage();
