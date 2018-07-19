c5f63a1a7dac3c11203e14b27f0c298b0b28ac25
Sander <github@ugoku.nl>
2018-07-19 20:34:21 +0200

    Partial cleanup for level fruit

--- levelfruit.php
+++ levelfruit.php
@@ -10,28 +10,93 @@ $palette = $paletteModel->palette;
 $handle = fopen('YOGHODAT.DAT', 'rb');
 fseek($handle, 13164);
 
-$wh = [
-    1 => [19, 17],
-    2 => [19, 17],
-    3 => [19, 17],
-    4 => [18, 16],
-    5 => [19, 16],
-    6 => [16, 17],
+$orderLevels1Through3 = [
+    0, 4, 8, 12, 16,
+    1, 5, 9, 13, 17,
+    2, 6, 10, 14, 18,
+    3, 7, 11, 15
+];
+$orderLevel4 = [
+    0, 4, 8, 12, 16,
+    1, 5, 9, 13, 17,
+    2, 6, 10, 14,
+    3, 7, 11, 15
+];
+$orderLevel5 = [
+    0, 4, 8, 12, 16,
+    1, 5, 9, 13, 17,
+    2, 6, 10, 14, 18,
+    3, 7, 11, 15
+];
+$orderLevel6 = [
+    0, 4, 8, 12,
+    1, 5, 9, 13,
+    2, 6, 10, 14,
+    3, 7, 11,
 ];
 
-$w1 = 19;
-$h1 = 17;
+$levels = [
+    1 => [
+        'w' => 19,
+        'h' => 17,
+        'order' => $orderLevels1Through3,
+    ],
+    2 => [
+        'w' => 19,
+        'h' => 17,
+        'order' => $orderLevels1Through3,
+    ],
+    3 => [
+        'w' => 19,
+        'h' => 17,
+        'order' => $orderLevels1Through3,
+    ],
+    4 => [
+        'w' => 18,
+        'h' => 16,
+        'order' => $orderLevel4,
+    ],
+    5 => [
+        'w' => 19,
+        'h' => 16,
+        'order' => $orderLevel5,
+    ],
+    6 => [
+        'w' => 16,
+        'h' => 17,
+        'order' => $orderLevel6,
+    ],
+];
 
-$w2 = 18;
-$h2 = 16;
-
-$w3 = 15;
-$h3 = 17;
-
-$levels = 6;
-
-$plaatje = imagecreatetruecolor(($w1 * 3) + ($w2 * 3), max($h1, $h2));
+$width = 0;
+$height = 0;
+foreach ($levels as $level) {
+    $width += $level['w'];
+    $height = max($height, $level['h']);
+}
+$plaatje = imagecreatetruecolor($width, $height);
 imagecolortransparent($plaatje, imagecolorallocate($plaatje, $palette[0][0], $palette[0][1], $palette[0][2]));
+
+$baseX = 0;
+foreach ($levels as $level) {
+    foreach ($level['order'] as $x) {
+        for ($y = 0; $y < $level['h']; $y++) {
+            $n = ord(fread($handle, 1));
+            $col = imagecolorallocate($plaatje, $palette[$n][0], $palette[$n][1], $palette[$n][2]);
+            imagesetpixel($plaatje, $baseX + $x, $y, $col);
+        }
+    }
+
+    $baseX += $level['w'];
+}
+
+
+fclose($handle);
+
+header("Content-type: image/png");
+imagepng($plaatje);
+
+
 /*
 $baseX = 0;
 foreach ($wh as list($w, $h)) {
@@ -54,86 +119,3 @@ foreach ($wh as list($w, $h)) {
 
     $baseX += $w;
 }*/
-
-
-// This works perfectly
-for ($l = 0; $l < 3; $l++) {
-    $baseX = $l * $w1;
-
-    $xs = [
-        0, 4, 8, 12, 16,
-        1, 5, 9, 13, 17,
-        2, 6, 10, 14, 18,
-        3, 7, 11, 15
-    ];
-    foreach ($xs as $x) {
-        for ($y = 0; $y < $h1; $y++) {
-            $n = ord(fread($handle, 1));
-            $col = imagecolorallocate($plaatje, $palette[$n][0], $palette[$n][1], $palette[$n][2]);
-            imagesetpixel($plaatje, $baseX + $x, $y, $col);
-        }
-    }
-}
-
-
-// This works for l=3, but not 4 or 5
-$l = 3;
-$baseX = (3 * $w1);
-
-$xs = [
-    0, 4, 8, 12, 16,
-    1, 5, 9, 13, 17,
-    2, 6, 10, 14,
-    3, 7, 11, 15
-];
-foreach ($xs as $x) {
-    for ($y = 0; $y < $h2; $y++) {
-        $n = ord(fread($handle, 1));
-        $col = imagecolorallocate($plaatje, $palette[$n][0], $palette[$n][1], $palette[$n][2]);
-        imagesetpixel($plaatje, $baseX + $x, $y, $col);
-    }
-}
-
-
-// This works for l=4
-$l = 4;
-$baseX = (3 * $w1) + $w2;
-
-$xs = [
-    0, 4, 8, 12, 16,
-    1, 5, 9, 13, 17,
-    2, 6, 10, 14, 18,
-    3, 7, 11, 15
-];
-foreach ($xs as $x) {
-    for ($y = 0; $y < $h2; $y++) {
-        $n = ord(fread($handle, 1));
-        $col = imagecolorallocate($plaatje, $palette[$n][0], $palette[$n][1], $palette[$n][2]);
-        imagesetpixel($plaatje, $baseX + $x, $y, $col);
-    }
-}
-
-
-// This works for l=4
-$l = 5;
-$baseX = (3 * $w1) + $w2 + 19;
-
-$xs = [
-    0, 4, 8, 12,
-    1, 5, 9, 13,
-    2, 6, 10, 14,
-    3, 7, 11,
-];
-foreach ($xs as $x) {
-    for ($y = 0; $y < $h3; $y++) {
-        $n = ord(fread($handle, 1));
-        $col = imagecolorallocate($plaatje, $palette[$n][0], $palette[$n][1], $palette[$n][2]);
-        imagesetpixel($plaatje, $baseX + $x, $y, $col);
-    }
-}
-
-
-fclose($handle);
-
-header("Content-type: image/png");
-imagepng($plaatje);
